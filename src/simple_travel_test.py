5ee829e136d8e17aad0ddba7361c0d9293d99d8e
lnalice <rara9915@gmail.com>
2024-07-29 15:40:44 +0900

    Add parser for robot name (multi-robot)

--- src/simple_travel_test.py
+++ src/simple_travel_test.py
@@ -5,10 +5,14 @@ import actionlib
 from actionlib_msgs.msg import GoalStatus
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
+import argparse
+
 class SimpleTraveler:
-    def __init__(self):
+    def __init__(self, param):
         # Initialize ROS
         rospy.init_node('travel_node')
+
+        self.robot_name = param.identity
 
         # Move to target position
         """
@@ -22,14 +26,16 @@ class SimpleTraveler:
         # Come back home (move to initial position)
 
     def move_action(self):
-        client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+        move_base_topic = self.robot_name + '/' + 'move_base'
+        client = actionlib.SimpleActionClient(move_base_topic, MoveBaseAction)
 
         rospy.loginfo('Waiting for the action server to start')
+    
         client.wait_for_server()
 
         rospy.loginfo('Action server started, sending the goal')
         goal = MoveBaseGoal()
-        goal.target_pose.header.frame_id = 'map'
+        goal.target_pose.header.frame_id = self.identity + '/ ' + 'map'
         goal.target_pose.header.stamp = rospy.Time.now()
         
         # set position
@@ -53,7 +59,13 @@ class SimpleTraveler:
         else:
             rospy.loginfo('The base failed to move for some reason')
 
+parser = argparse.ArgumentParser(description="robot specification",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument(
+    '-i', '--identity', required=True, help='Robot Name (multi-robot)') # ex) tb3_1
+args = parser.parse_args()
+
 if __name__ == "__main__":
-    simple_traveler = SimpleTraveler()
+    simple_traveler = SimpleTraveler(param=args)
     simple_traveler.move_action()
     rospy.spin()
