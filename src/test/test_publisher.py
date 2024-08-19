0ed9f74870a6998c609dbfdbb150af6fb4984fe4
lnalice <rara9915@gmail.com>
2024-08-19 10:57:01 +0900

    Add cmd/names/scene in argparser

--- src/test/test_publisher.py
+++ src/test/test_publisher.py
@@ -11,7 +11,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="robot specification",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument(
-        '-n', '--name', required=False, help='Robot Name (multi-robot)') # ex) tb3_1
+        '-c', '--command', required=False,
+        help='command [MOVE, HOME, MODULE, SCENE]')
+    parser.add_argument(
+        '-n', '--names', required=False, nargs='+', help='Robot Name (multi-robot)') # ex) tb3_1
     parser.add_argument(
         '-s', '--scene', required=False, help='Scene ID')
     args = parser.parse_args()
@@ -47,12 +50,13 @@ if __name__ == "__main__":
     test_topic = '/react/commander'
     pub = rospy.Publisher(test_topic, String, queue_size=1)
 
-    act = "HOME"
+    command = args.command.upper()
     scene_id = args.scene
-    IDs = "tb3_0 tb3_5 tb3_1 tb3_2 tb3_3 tb3_4 tb3_7"
-    msg = act + " " + scene_id + " " + IDs
+    IDs = ' '.join(args.names)
+    
+    msg = command + " " + scene_id + " " + IDs
 
     rospy.sleep(0.1)
 
     pub.publish(msg)
-    rospy.loginfo("I sent topic %s", msg)
+    rospy.loginfo("I sent topic \"%s\"", msg)
