e6da1c72dc47d4666f7ebfc5ffa5ef13e1cc4129
lnalice <rara9915@gmail.com>
2024-12-10 14:55:11 +0900

    Delete unused codes & files

--- src/robot_planner.py
+++ src/robot_planner.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
 
 import rospy
-import actionlib
-from actionlib_msgs.msg import GoalStatus
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from std_msgs.msg import String
 
 import os
-import argparse
 import math
 
-from publisher.moveBasePub import moveByBase
 from publisher.cmdVelPub import moveByVel
-from publisher.ctrlModulePub import ctrlByVel, ctrlByPos
+from publisher.ctrlModulePub import ctrlByPos
 
 from publisher.blinkLedPub import blinkLed
 
@@ -22,27 +17,20 @@ STOP_SECONDS = 2
 SPIN_ONCE_SEC = 10
 SPIN_ONCE_LIN = (0, 0, 0)
 SPIN_ONCE_ANG = (0, 0, 2*math.pi / SPIN_ONCE_SEC)
+
 DIR_ADJUSTMENT_SEC = 3
 COMEBACK_VEL = 0.15
 
 class Traveler:
     def __init__(self):
 
-        """
-        how to set robot name
-        1. use argeparser
-        2. use rospy.get_param
-        3. use env [selected]
-        """
-        # self.robot_name = param.name
-        # self.robot_name = rospy.get_param("multi_robot_name")
         self.robot_name = os.getenv('ROBOT_NAME', 'tb3_#')
 
         rospy.loginfo("**Robot name is... " + self.robot_name)
 
         rospy.init_node('robot_planner_node_'+ self.robot_name)
 
-        #rotation recovery (for localization): rotation once
+        # rotation recovery (for localization): rotation once
         # moveByVel(self.robot_name, SPIN_ONCE_SEC, SPIN_ONCE_LIN, SPIN_ONCE_ANG)
 
         # Move to target position
@@ -52,14 +40,6 @@ class Traveler:
         # Take a spin on the spot
         rospy.Subscriber("/scene_manager/ctrl_module_req", String, self.ctrl_module, queue_size=1)
         self.ctrl_module_res_pub =rospy.Publisher('/scene_manager/ctrl_module_res', String, queue_size=1)
-
-        # Come back home (move to initial position)
-        rospy.Subscriber("/scene_manager/go_home", String, self.go_home, queue_size=1)
-        self.come_back_pub = rospy.Publisher('/scene_manager/come_back_home', String, queue_size=1)
-        
-        # /cmd_vel
-        self.displacement_x :float = 0.0
-        self.displacement_z :float = 0.0
 
         rospy.loginfo('[RobotPlanner-%s] I\'m ready!', self.robot_name)
 
@@ -74,46 +54,34 @@ class Traveler:
         
         rospy.sleep(0.1)
 
-        # /move_base
-        if len(req_list) <= 5:
-            pos_x, pos_y, ort_z, ort_w = req_list[1:]
-            if moveByBase(self.robot_name, (pos_x, pos_y), (ort_z, ort_w)):
-                self.move_res_pub.publish(req_data.data)
-            else:
-                rospy.logerr("[RobotPlanner-%s] Failed! (/move_base)", req_id)
-
         # /cmd_vel
-        else: 
-            seconds = int(req_list[1])
-            lin_vel = tuple(float(e) for e in req_list[2:5])
-            ang_vel = tuple(float(e) for e in req_list[5:8])
-            delay_sec = float(req_list[8])
+        seconds = int(req_list[1])
+        lin_vel = tuple(float(e) for e in req_list[2:5])
+        ang_vel = tuple(float(e) for e in req_list[5:8])
+        delay_sec = float(req_list[8])
 
-            if req_id != self.robot_name:
-                rospy.loginfo("this ID(%s) is not mine.", req_id)
-                return
+        if req_id != self.robot_name:
+            rospy.loginfo("this ID(%s) is not mine.", req_id)
+            return
             
-            self.displacement_x += lin_vel[0] * seconds
-            self.displacement_z += ang_vel[2] * seconds
-            
-            rospy.sleep(int(delay_sec))
-            rospy.logwarn("[RobotPlanner-%s] now this robot is moving...\n\n", req_id)
+        rospy.sleep(int(delay_sec))
+        rospy.logwarn("[RobotPlanner-%s] now this robot is moving...\n\n", req_id)
 
-            try:
-                blinkLed(self.robot_name, vel2statusByte(lin_vel[0], ang_vel[2])) # blink LED
-                moveByVel(self.robot_name, seconds, lin_vel, ang_vel) # move using cmd_vel
+        try:
+            blinkLed(self.robot_name, vel2statusByte(lin_vel[0], ang_vel[2])) # blink LED
+            moveByVel(self.robot_name, seconds, lin_vel, ang_vel) # move using cmd_vel
                 
-                # <recovery> direction adjustment using cmd_vel
-                if ang_vel[-1] != 0:
-                    _ang_vel = (-ang_vel[-1] * seconds) / DIR_ADJUSTMENT_SEC
-                    moveByVel(self.robot_name, DIR_ADJUSTMENT_SEC, (0.0, 0.0, 0.0), (0.0, 0.0, _ang_vel))
+            # <recovery> direction adjustment using cmd_vel
+            if ang_vel[-1] != 0:
+                _ang_vel = (-ang_vel[-1] * seconds) / DIR_ADJUSTMENT_SEC
+                moveByVel(self.robot_name, DIR_ADJUSTMENT_SEC, (0.0, 0.0, 0.0), (0.0, 0.0, _ang_vel))
 
-            except:
-                rospy.logerr("[RobotPlanner-%s] Failed! (/cmd_vel)", req_id)
+        except:
+            rospy.logerr("[RobotPlanner-%s] Failed! (/cmd_vel)", req_id)
 
-            finally:
-                moveByVel(self.robot_name, STOP_SECONDS, (0.0, 0.0, 0.0), (0.0, 0.0, 0.0))
-                self.move_res_pub.publish(req_data)
+        finally:
+            moveByVel(self.robot_name, STOP_SECONDS, (0.0, 0.0, 0.0), (0.0, 0.0, 0.0))
+            self.move_res_pub.publish(req_data)
         
     def ctrl_module(self, req_data):
 
@@ -138,40 +106,7 @@ class Traveler:
         finally:
             self.ctrl_module_res_pub.publish(req_data)
 
-    def go_home(self, req_data):
-        if str(req_data.data) != self.robot_name:
-            rospy.loginfo("[RobotPlanner-%s] This id(%s) is not mine.", self.robot_name, req_data.data)
-            return
-        seconds = self.displacement_x / COMEBACK_VEL
-        ang_vel = 0 if seconds == 0 else self.displacement_z / seconds
-
-        self.displacement_x = 0
-        self.displacement_z = 0
-
-        rospy.logwarn("[RobotPlanner-%s] now this robot is moving...\n\n", self.robot_name)
-        
-        try:
-            moveByVel(self.robot_name, seconds, (-COMEBACK_VEL, 0.0, 0.0), (0.0, 0.0, ang_vel))
-            # direction adjustment using cmd_vel
-            if ang_vel[-1] != 0:
-                _ang_vel = (-ang_vel[-1] * seconds) / DIR_ADJUSTMENT_SEC
-                moveByVel(self.robot_name, DIR_ADJUSTMENT_SEC, (0.0, 0.0, 0.0), (0.0, 0.0, _ang_vel))
-
-        except:
-            rospy.logerr("[RobotPlanner-%s] Failed! (/cmd_vel)", self.robot_name)
-
-        finally:
-            moveByVel(self.robot_name, STOP_SECONDS, (0.0, 0.0, 0.0), (0.0, 0.0, 0.0))
-            self.come_back_pub.publish(req_data)
-
-
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser(description="robot specification",
-    #                              formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    # parser.add_argument(
-    #     '-n', '--name', required=True, help='Robot Name (multi-robot)') # ex) tb3_1
-    # args = parser.parse_args()
-    
-    # simple_traveler = Traveler(param=args)
     simple_traveler = Traveler()
+
     rospy.spin()
